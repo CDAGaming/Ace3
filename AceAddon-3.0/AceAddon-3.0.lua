e7d7d3d6b0aa61805d4df9f76d14236f1b0721c0
ammo <ammo@5debad98-a965-4143-8383-f471b3509dcf>
2007-09-02 20:20:29 +0000

    Ace3: AceAddon-3.0 - spaces -> tabs again
    git-svn-id: https://repos.curseforge.com/wow/ace3/branches/Ace3/Alpha@11 5debad98-a965-4143-8383-f471b3509dcf

--- AceAddon-3.0/AceAddon-3.0.lua
+++ AceAddon-3.0/AceAddon-3.0.lua
@@ -7,7 +7,7 @@ if not AceAddon then
 elseif not oldminor then -- This is the first version
 	AceAddon.frame = CreateFrame("Frame", "AceAddon30Frame") -- Our very own frame
 	AceAddon.addons = {} -- addons in general
-    AceAddon.statuses = {} -- statuses of addon.  
+	AceAddon.statuses = {} -- statuses of addon.  
 	AceAddon.initializequeue = {} -- addons that are new and not initialized
 	AceAddon.enablequeue = {} -- addons that are initialized and waiting to be enabled
 	AceAddon.embeds = setmetatable({}, {__index = function(tbl, key) tbl[key] = {} return tbl[key] end }) -- contains a list of libraries embedded in an addon
@@ -104,15 +104,15 @@ end
 -- calls OnEnable on the addon object if available
 -- calls OnEmbedEnable on embedded libs in the addon object if available
 function AceAddon:EnableAddon( addon )
-    if self.statuses[addon.name] then return false end
+	if self.statuses[addon.name] then return false end
 	-- TODO: handle 'first'? Or let addons do it on their own?
 	safecall(addon.OnEnable, addon)
 	for k, libname in ipairs(self.embeds[addon]) do
 		local lib = LibStub:GetLibrary(libname, true)
 		if lib then safecall(lib.OnEmbedEnable, lib, addon) end
 	end
-    self.statuses[addon.name] = true
-    return true
+	self.statuses[addon.name] = true
+	return true
 end
 
 -- AceAddon:DisableAddon( addon )
@@ -121,14 +121,14 @@ end
 -- calls OnDisable on the addon object if available
 -- calls OnEmbedDisable on embedded libs in the addon object if available
 function AceAddon:DisableAddon( addon )
-    if not self.statuses[addon.name] then return false end
+	if not self.statuses[addon.name] then return false end
 	safecall( addon.OnDisable, addon )
-    for k, libname in ipairs(self.embeds[addon]) do
-        local lib = LibStub:GetLibrary(libname, true)
-        if lib then safecall(lib.OnEmbedDisable, lib, addon) end
-    end
-    self.statuses[addon.name] = nil
-    return true
+	for k, libname in ipairs(self.embeds[addon]) do
+		local lib = LibStub:GetLibrary(libname, true)
+		if lib then safecall(lib.OnEmbedDisable, lib, addon) end
+	end
+	self.statuses[addon.name] = nil
+	return true
 end
 
 -- Event Handling
