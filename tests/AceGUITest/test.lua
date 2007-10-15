a20e6d7c36cd0eadd4d0bd41475bd52174d40881
nargiddley <nargiddley@5debad98-a965-4143-8383-f471b3509dcf>
2007-10-15 09:22:57 +0000

    Ace3 - AceGUITest: Update for new Tree format, more tests
    git-svn-id: https://repos.curseforge.com/wow/ace3/branches/Ace3/Alpha@204 5debad98-a965-4143-8383-f471b3509dcf

--- tests/AceGUITest/test.lua
+++ tests/AceGUITest/test.lua
@@ -1,7 +1,78 @@
 local AceGUI = LibStub("AceGUI-3.0")
 
 local function print(a)
-    DEFAULT_CHAT_FRAME:AddMessage(a)
+	DEFAULT_CHAT_FRAME:AddMessage(a)
+end
+
+
+local function ZOMGConfig(widget, event)
+	AceGUI:Release(widget.userdata.parent)
+	
+	local f = AceGUI:Create("Frame")
+	
+	f:SetCallback("OnClose",function(widget, event) print("Closing") AceGUI:Release(widget) end )
+	f:SetTitle("ZOMG Config!")
+	f:SetStatusText("Status Bar")
+	f:SetLayout("Fill")
+	
+	local maingroup = AceGUI:Create("DropdownGroup")
+	maingroup:SetLayout("Fill")
+	maingroup:SetGroupList({Addons = "Addons !!", Zomg = "Zomg Addons"})
+	maingroup:SetGroup("Addons")
+	maingroup:SetTitle("")
+	
+	f:AddChild(maingroup)
+	
+	local tree = { "A", "B", "C", "D", B = { "B1", "B2", B1 = { "B11", "B12" } }, C = { "C1", "C2", C1 = { "C11", "C12" } } }
+	local text = { A = "Option 1", B = "Option 2", C = "Option 3", D = "Option 4", J = "Option 10", K = "Option 11", L = "Option 12", 
+					B1 = "Option 2-1", B2 = "Option 2-2", B11 = "Option 2-1-1", B12 = "Option 2-1-2",
+					C1 = "Option 3-1", C2 = "Option 3-2", C11 = "Option 3-1-1", C12 = "Option 3-1-2" }
+	local t = AceGUI:Create("TreeGroup")
+	t:SetLayout("Fill")
+	t:SetTree(tree, text)
+	maingroup:AddChild(t)
+	
+	local tab = AceGUI:Create("TabGroup")
+	tab:SetTabs({"A","B","C","D"},{A="Yay",B="We",C="Have",D="Tabs"})
+	tab:SetLayout("Fill")
+	tab:SelectTab(1)
+	t:AddChild(tab)
+	
+	local component = AceGUI:Create("DropdownGroup")
+	component:SetLayout("Fill")
+	component:SetGroupList({Blah = "Blah", Splat = "Splat"})
+	component:SetGroup("Blah")
+	component:SetTitle("Choose Componet")
+	
+	tab:AddChild(component)
+	
+	local more = AceGUI:Create("DropdownGroup")
+	more:SetLayout("Fill")
+	more:SetGroupList({ButWait = "But Wait!", More = "Theres More"})
+	more:SetGroup("More")
+	more:SetTitle("And More!")
+	
+	component:AddChild(more)
+	
+	local sf = AceGUI:Create("ScrollFrame")
+	sf:SetLayout("Flow")
+	more:AddChild(sf)
+	local stuff = AceGUI:Create("Heading")
+	stuff:SetText("Omg Stuff Here")
+	stuff.width = "fill"
+	sf:AddChild(stuff)
+	
+	for i = 1, 10 do
+		local edit = AceGUI:Create("EditBox")
+		edit:SetText("")
+		edit:SetWidth(200)
+		edit:SetLabel("Stuff!")
+		edit:SetCallback("OnEnterPressed",function(widget,event,text) widget:SetLabel(text) end )
+		edit:SetCallback("OnTextChanged",function(widget,event,text) print(text) end )
+		sf:AddChild(edit)
+	end
+	
+	f:Show()
 end
 
 local function GroupA(content)
@@ -11,19 +82,49 @@ local function GroupA(content)
 	sf:SetLayout("Flow")
 	
 	local edit = AceGUI:Create("EditBox")
-    edit:SetText("Testing")
-    edit:SetWidth(200)
-    edit:SetLabel("Group A Option")
-    edit:SetCallback("OnEnterPressed",function(widget,event,text) widget:SetLabel(text) end )
-    edit:SetCallback("OnTextChanged",function(widget,event,text) print(text) end )
-    sf:AddChild(edit)
-    
-    local slider = AceGUI:Create("Slider")
-    slider:SetLabel("Group A Slider")
-    slider:SetSliderValues(0,1000,5)
-    slider:SetDisabled(false)
-    sf:AddChild(slider)
-    
+	edit:SetText("Testing")
+	edit:SetWidth(200)
+	edit:SetLabel("Group A Option")
+	edit:SetCallback("OnEnterPressed",function(widget,event,text) widget:SetLabel(text) end )
+	edit:SetCallback("OnTextChanged",function(widget,event,text) print(text) end )
+	sf:AddChild(edit)
+	
+	local slider = AceGUI:Create("Slider")
+	slider:SetLabel("Group A Slider")
+	slider:SetSliderValues(0,1000,5)
+	slider:SetDisabled(false)
+	sf:AddChild(slider)
+	
+	local zomg = AceGUI:Create("Button")
+	zomg.userdata.parent = content.userdata.parent
+	zomg:SetText("Zomg!")
+	zomg:SetCallback("OnClick", ZOMGConfig)
+	sf:AddChild(zomg)
+	
+	local heading1 = AceGUI:Create("Heading")
+	heading1:SetText("Heading 1")
+	heading1.width = "fill"
+	sf:AddChild(heading1)
+	
+	for i = 1, 5 do
+		local radio = AceGUI:Create("CheckBox")
+		radio:SetLabel("Test Check "..i)
+		radio:SetCallback("OnValueChanged",function(widget,event,value) print(value and "Check "..i.." Checked" or "Check "..i.." Unchecked") end )
+		sf:AddChild(radio)
+	end
+	
+	local heading2 = AceGUI:Create("Heading")
+	heading2:SetText("Heading 2")
+	heading2.width = "fill"
+	sf:AddChild(heading2)
+	
+	for i = 1, 5 do
+		local radio = AceGUI:Create("CheckBox")
+		radio:SetLabel("Test Check "..i+5)
+		radio:SetCallback("OnValueChanged",function(widget,event,value) print(value and "Check "..i.." Checked" or "Check "..i.." Unchecked") end )
+		sf:AddChild(radio)
+	end
+	
 	local heading1 = AceGUI:Create("Heading")
 	heading1:SetText("Heading 1")
 	heading1.width = "fill"
@@ -57,17 +158,17 @@ local function GroupB(content)
 	sf:SetLayout("Flow")
 	
  	local check = AceGUI:Create("CheckBox")
-    check:SetLabel("Group B Checkbox")
-    check:SetCallback("OnValueChanged",function(widget,event,value) print(value and "Checked" or "Unchecked") end )
-    
-    local dropdown = AceGUI:Create("Dropdown")
-    dropdown:SetText("Test")
-    dropdown:SetLabel("Group B Dropdown")
-    dropdown.list = {"Test","Test2"}
-    dropdown:SetCallback("OnValueChanged",function(widget,event,value) print(value) end )
-    
-    sf:AddChild(check)
-    sf:AddChild(dropdown)
+	check:SetLabel("Group B Checkbox")
+	check:SetCallback("OnValueChanged",function(widget,event,value) print(value and "Checked" or "Unchecked") end )
+	
+	local dropdown = AceGUI:Create("Dropdown")
+	dropdown:SetText("Test")
+	dropdown:SetLabel("Group B Dropdown")
+	dropdown.list = {"Test","Test2"}
+	dropdown:SetCallback("OnValueChanged",function(widget,event,value) print(value) end )
+	
+	sf:AddChild(check)
+	sf:AddChild(dropdown)
 	content:AddChild(sf)
 end
 
@@ -78,14 +179,14 @@ local function OtherGroup(content)
 	sf:SetLayout("Flow")
 	
  	local check = AceGUI:Create("CheckBox")
-    check:SetLabel("Test Check")
-    check:SetCallback("OnValueChanged",function(widget,event,value) print(value and "CheckButton Checked" or "CheckButton Unchecked") end )
-    
-    sf:AddChild(check)
-    
-    local inline = AceGUI:Create("InlineGroup")
-    inline:SetLayout("Flow")
-    inline:SetTitle("Inline Group")
+	check:SetLabel("Test Check")
+	check:SetCallback("OnValueChanged",function(widget,event,value) print(value and "CheckButton Checked" or "CheckButton Unchecked") end )
+	
+	sf:AddChild(check)
+	
+	local inline = AceGUI:Create("InlineGroup")
+	inline:SetLayout("Flow")
+	inline:SetTitle("Inline Group")
 	inline.width = "fill"
 
 	local heading1 = AceGUI:Create("Heading")
@@ -93,12 +194,12 @@ local function OtherGroup(content)
 	heading1.width = "fill"
 	inline:AddChild(heading1)
 	
-    for i = 1, 10 do
-	    local radio = AceGUI:Create("CheckBox")
-	    radio:SetLabel("Test Radio "..i)
-	    radio:SetCallback("OnValueChanged",function(widget,event,value) print(value and "Radio "..i.." Checked" or "Radio "..i.." Unchecked") end )
-	    radio:SetType("radio")
-	    inline:AddChild(radio)
+	for i = 1, 10 do
+		local radio = AceGUI:Create("CheckBox")
+		radio:SetLabel("Test Radio "..i)
+		radio:SetCallback("OnValueChanged",function(widget,event,value) print(value and "Radio "..i.." Checked" or "Radio "..i.." Unchecked") end )
+		radio:SetType("radio")
+		inline:AddChild(radio)
 	end
 	
 	local heading2 = AceGUI:Create("Heading")
@@ -106,12 +207,12 @@ local function OtherGroup(content)
 	heading2.width = "fill"
 	inline:AddChild(heading2)
 	
-    for i = 1, 10 do
-	    local radio = AceGUI:Create("CheckBox")
-	    radio:SetLabel("Test Radio "..i)
-	    radio:SetCallback("OnValueChanged",function(widget,event,value) print(value and "Radio "..i.." Checked" or "Radio "..i.." Unchecked") end )
-	    radio:SetType("radio")
-	    inline:AddChild(radio)
+	for i = 1, 10 do
+		local radio = AceGUI:Create("CheckBox")
+		radio:SetLabel("Test Radio "..i)
+		radio:SetCallback("OnValueChanged",function(widget,event,value) print(value and "Radio "..i.." Checked" or "Radio "..i.." Unchecked") end )
+		radio:SetType("radio")
+		inline:AddChild(radio)
 	end
 	
 	
@@ -121,24 +222,51 @@ end
 
 local function SelectGroup(widget, event, value)
 	if value == "A" then
-        GroupA(widget)
-    elseif value == "B" then
-       GroupB(widget)
-    else
-    	OtherGroup(widget)
-    end
+		GroupA(widget)
+	elseif value == "B" then
+	 GroupB(widget)
+	else
+		OtherGroup(widget)
+	end
 end
 
 
 local function TreeWindow(content)
 	content:ReleaseChildren()
 	
-	local tree = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", B = { "B1", "B2", B1 = { "B11", "B12" } } }
-	local text = { A = "Option 1", B = "Option 2", C = "Option 3", D = "Option 4", E = "Option 5", F = "Option 6", G = "Option 7", H = "Option 8", I = "Option 9",
-					J = "Option 10", K = "Option 11", L = "Option 12", B1 = "Option 2-1", B2 = "Option 2-2", B11 = "Option 2-1-1", B12 = "Option 2-1-2" }
+	local tree = { 
+			{ 
+				value = "A",
+				text = "Alpha"
+			},
+			{
+				value = "B",
+				text = "Bravo",
+				children = {
+					{ 
+						value = "C", 
+						text = "Charlie",
+					},
+					{
+						value = "D",	
+						text = "Delta",
+						children = { 
+							{ 
+								value = "E",
+								text = "Echo",
+							} 
+						} 
+					},
+				}
+			},
+			{ 
+				value = "F", 
+				text = "Foxtrot",
+			},
+		}
 	local t = AceGUI:Create("TreeGroup")
 	t:SetLayout("Fill")
-	t:SetTree(tree, text)
+	t:SetTree(tree)
 	t:SetCallback("OnGroupSelected", SelectGroup )
 	content:AddChild(t)
 	SelectGroup(t,"OnGroupSelected","A")
@@ -148,24 +276,26 @@ end
 local function TabWindow(content)
 	content:ReleaseChildren()
 	local tab = AceGUI:Create("TabGroup")
-    tab:SetTabs({"A","B","C","D"},{A="Alpha",B="Bravo",C="Charlie",D="Deltaaaaaaaaaaaaaa"})
-    tab:SetTitle("Tab Group")
-    tab:SetLayout("Fill")
-    tab:SetCallback("OnGroupSelected",SelectGroup)
-    tab:SelectTab(1)
-    content:AddChild(tab)
+	tab.userdata.parent = content.userdata.parent
+	tab:SetTabs({"A","B","C","D"},{A="Alpha",B="Bravo",C="Charlie",D="Deltaaaaaaaaaaaaaa"})
+	tab:SetTitle("Tab Group")
+	tab:SetLayout("Fill")
+	tab:SetCallback("OnGroupSelected",SelectGroup)
+	tab:SelectTab(1)
+	content:AddChild(tab)
 	
 end
 
 
 function TestFrame()
-    local f = AceGUI:Create("Frame")
-    f:SetCallback("OnClose",function(widget, event) print("Closing") AceGUI:Release(widget) end )
-    f:SetTitle("AceGUI Prototype")
-    f:SetStatusText("Root Frame Status Bar")
-    f:SetLayout("Fill")
-    
+	local f = AceGUI:Create("Frame")
+	f:SetCallback("OnClose",function(widget, event) print("Closing") AceGUI:Release(widget) end )
+	f:SetTitle("AceGUI Prototype")
+	f:SetStatusText("Root Frame Status Bar")
+	f:SetLayout("Fill")
+	
 	local maingroup = AceGUI:Create("DropdownGroup")
+	maingroup.userdata.parent = f
 	maingroup:SetLayout("Fill")
 	maingroup:SetGroupList({Tab = "Tab Frame", Tree = "Tree Frame"})
 	maingroup:SetGroup("Tab")
@@ -185,5 +315,4 @@ function TestFrame()
 	
 	f:Show()
 end
-
-TestFrame()
+--TestFrame()
