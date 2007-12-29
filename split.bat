a6c2312a1f5d9d56c1958ff02c9b898b6684d392
funkydude <funkydude@5debad98-a965-4143-8383-f471b3509dcf>
2007-12-29 13:24:59 +0000

    Ace3 - fix splitting
    git-svn-id: https://repos.curseforge.com/wow/ace3/branches/Ace3/Beta@426 5debad98-a965-4143-8383-f471b3509dcf

--- split.bat
+++ split.bat
@@ -7,7 +7,7 @@ echo Splitting off %1...
 echo %1 >> split.txt
 echo ##Interface: 20300 > %1\%1.toc
 echo ##Title:Lib:%1 >> %1\%1.toc
-if not "%1" == "CallbackHandler-1.0" echo ##OptionalDeps:CallbackHandler-1.0 >> %1\%1.toc
+if not "%1" == "CallbackHandler-1.0" echo ##OptionalDeps:CallbackHandler-1.0, AceGUI-3.0, AceConsole-3.0 >> %1\%1.toc
 echo ##LoadWith:Ace3 >> %1\%1.toc
 echo %1.xml >> %1\%1.toc
 move %1 ..
